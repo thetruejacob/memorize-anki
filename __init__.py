63ae92c9bc4e33d9020d67d57c76980137be187a
Ross Todd <rssmtdd@gmail.com>
2020-11-30 06:27:32 -0800

    Fix | Update constant and automatic formatting fixes my editor threw in

--- __init__.py
+++ __init__.py
@@ -13,6 +13,7 @@ import time
 # import the "show info" tool from utils.py
 from aqt.utils import showInfo
 
+
 def answerButtons(self, card):
     return 2
 
@@ -23,9 +24,11 @@ SchedulerV2.answerButtons = answerButtons
 oldFlush = Card.flush
 oldFlushSched = Card.flushSched
 
+
 def reprocess(card):
     currentSecond = time.time()
-    lastReviewSecond = card.col.db.scalar("SELECT id FROM revlog WHERE cid = ? ORDER BY id DESC", card.id) // 1000
+    lastReviewSecond = card.col.db.scalar(
+        "SELECT id FROM revlog WHERE cid = ? ORDER BY id DESC", card.id) // 1000
     ivlInHour = ResultsandTimes(card.id)
     ivlInDay = round(ivlInHour/24)
     ivlInSecond = ivlInHour * 3600
@@ -43,10 +46,11 @@ def reprocess(card):
     if nextReviewSecond <= currentSecond:
         # card is already due
         card.queue = QUEUE_TYPE_REV
-        card.type = CARD_TYPE_REV 
+        card.type = CARD_TYPE_REV
         showInfo(f"This card is overdue. The next interval will be {card.ivl}")
-        card.ivl = self.today # set to something else? Originally card.ivl = ivlInHour
-        card.due = card.col.sched.today # TODO: find real day  # Originally card.due = self.today
+        card.ivl = self.today  # set to something else? Originally card.ivl = ivlInHour
+        # TODO: find real day  # Originally card.due = self.today
+        card.due = card.col.sched.today
         # showInfo(f"Setting its due date to today since already due.")
         return
 
@@ -73,23 +77,31 @@ def flush(self):
         # Only consider review and learning
         reprocess(self)
     oldFlush(self)
+
+
 Card.flush = flush
+
+
 def flushSched(self):
     print("our flush")
     if self.queue in {QUEUE_TYPE_LRN, QUEUE_TYPE_DAY_LEARN_RELEARN, QUEUE_TYPE_REV}:
         # Only consider review and learning
         reprocess(self)
     oldFlushSched(self)
+
+
 Card.flushSched = flushSched
 
+
 def ResultsandTimes(cardID, initialModel=ebisuAllInOne.defaultModel(24, 3)):
-    ''' 
+    '''
     Takes as input the cardID and initial guess of the model, and returns the next interval in hours from the last interval
     The default model assumes a beta distribution with alpha = beta = 3, half life = 24 hours.
     '''
 
     # pull reviewresults from database
-    reviewResults = mw.col.db.list("SELECT ease FROM revlog WHERE cid = ?", cardID) 
+    reviewResults = mw.col.db.list(
+        "SELECT ease FROM revlog WHERE cid = ?", cardID)
 
     # format properly: ease is encoded as 1(wrong), 2(hard), 3(ok), 4(easy) for review, and  1(wrong), 2(ok), 3(easy) for learn/relearn
     # just convert it to a list of True/False corresponding to review result
@@ -99,36 +111,38 @@ def ResultsandTimes(cardID, initialModel=ebisuAllInOne.defaultModel(24, 3)):
     reviewTimes = mw.col.db.list("SELECT id FROM revlog WHERE cid = ?", cardID)
 
     # format properly: id is actually the epoch-millisecond timestamp when the review was performed
-    reviewTimes = [datetime.fromtimestamp(time / 1000.00) for time in reviewTimes]
+    reviewTimes = [datetime.fromtimestamp(
+        time / 1000.00) for time in reviewTimes]
 
     previousTime, model = reviewTimes[0], initialModel
 
     # showInfo(f"Review Results are {reviewResults},\n Review Times are {reviewTimes},\n previousTime is {previousTime},\n model is {model}")
 
-
-    # Here, we start updating the recall successively based on the entire review history of the card. 
+    # Here, we start updating the recall successively based on the entire review history of the card.
     for i in range(1, len(reviewTimes)):
         # showInfo(f"Review {i} at {reviewTimes[i]}")
-        timedifference_hours = (reviewTimes[i] - reviewTimes[i-1]).total_seconds()/3600
-        
+        timedifference_hours = (
+            reviewTimes[i] - reviewTimes[i-1]).total_seconds()/3600
+
         # showInfo(f"Elapsed {round(timedifference_hours,2)} hours\nPredicted {predictRecall(model, timedifference_hours, exact = True)}\nResult: {reviewResults[i]} \n")
-        
-        model = a, b, t = ebisuAllInOne.updateRecall(prior = model, result = reviewResults[i], tnow = timedifference_hours)
+
+        model = a, b, t = ebisuAllInOne.updateRecall(
+            prior=model, result=reviewResults[i], tnow=timedifference_hours)
         # showInfo(f"Updated model: {round(a,2), round(b,2), round(t,2)} \nThe estimated half-life is {round(modelToPercentileDecay(model), 3)}")
 
-
-    # modelToPercentileDecay estimates how long (in hours) it will take for a model to decay to a given percentile 
+    # modelToPercentileDecay estimates how long (in hours) it will take for a model to decay to a given percentile
     # Here, the percentile is 0.5, which is the half-life of the memory.
     # showInfo(f"model to percentile decay is {ebisuAllInOne.modelToPercentileDecay(model)}")
     return ebisuAllInOne.modelToPercentileDecay(model)
 
+
 def ebisuAll():
-    for cid in mw.col.db.list(f"SELECT id FROM cards where queue in ({QUEUE_LRN}, {QUEUE_DAY_LRN}, {QUEUE_REV})"):
+    for cid in mw.col.db.list(f"SELECT id FROM cards where queue in ({QUEUE_TYPE_LRN}, {QUEUE_TYPE_DAY_LEARN_RELEARN}, {QUEUE_TYPE_REV})"):
         card = mw.col.getCard(cid)
         card.flush()
+
 
 action = QAction(mw)
 action.setText("Ebisu")
 mw.form.menuTools.addAction(action)
 action.triggered.connect(ebisuAll)
-
